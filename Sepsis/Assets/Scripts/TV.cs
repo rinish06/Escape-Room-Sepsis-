76fc81fcdc8df67ecb314b2f3052382de423453c
Christian Berck <christian@berck.org>
2020-02-26 17:07:03 -0600

    Tv text now changes after given time and imported clipboard

--- Sepsis/Assets/Scripts/TV.cs
+++ Sepsis/Assets/Scripts/TV.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.UI;
+using TMPro;
 
 public class TV : MonoBehaviour
 {
+    public TextMeshPro temp;
     public GameObject tvScreen;
     // Start is called before the first frame update
     void Start()
     {
         
+        StartCoroutine(TutorialSteps());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator TutorialSteps(){
+        yield return new WaitForSeconds(5);
+        tvScreen.GetComponent<TextMeshPro>().text = "First we are going to show you how to move around the room";
     }
 }
