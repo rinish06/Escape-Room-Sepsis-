d4f7e4844b4066b610fdf7d81c2c241c7282ac86
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-03-02 16:30:52 -0600

    Added sphere dissapearing script
    Added sphere dissapearing script

---
+++ Sepsis/Assets/Scripts/BallCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Hand"))
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+}
