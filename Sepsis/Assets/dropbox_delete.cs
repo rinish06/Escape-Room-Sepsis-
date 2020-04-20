aec1f15bc136687893b849b309120b006d7ebcd2
WChandler432 <wyster432@gmail.com>
2020-04-20 11:27:27 -0500

    Added lab forms, bookshelf, books, dropbox with object deletion, added IV bag without stand

---
+++ Sepsis/Assets/dropbox_delete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropbox_delete : MonoBehaviour
+{
+  void OnTriggerEnter(Collider BookCollider){
+      if(col.tag == "Book"){
+           Destroy(BookCollider.gameObject);
+      }
+ }
+}
